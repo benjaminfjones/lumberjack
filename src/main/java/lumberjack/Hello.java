e4fe3c534e69262ff8a76582a0963ca01d156160
Benjamin Jones <benjaminfjones@gmail.com>
2020-03-28 21:41:40 -0700

    Initial commit of a working Gradle project

---
+++ src/main/java/lumberjack/Hello.java
@@ -0,0 +1,11 @@
+package lumberjack;
+
+class Hello {
+        public static void hello() {
+                System.out.println("Hello, World!");
+        }
+
+        public static boolean returnTrue() {
+                return true;
+        }
+}
